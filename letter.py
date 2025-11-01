0de55ca5c09448a65eb6e735577dad8e06d6501b
Mpilo Mnyandu <mpmnyswgc025@student.wethinkcode.co.za>
2025-11-01 09:57:21 +0200

    Complete i think

--- letter.py
+++ letter.py
@@ -1,2 +1,32 @@
 def letter_grade(mark:int) -> str:
-    return 'a'
+    A=[]
+    B=[]
+    C =[]
+    D=[]
+    F=[]
+
+    for i in range(80,101):
+        A.append(i)
+
+    for i in range(70,80):
+        B.append(i)
+
+    for i in range(60,70):
+        C.append(i)
+
+    for i in range(50,60):
+        D.append(i)
+
+    for i in range(0,50):
+        F.append(i)
+
+    if mark in A:
+        return 'A'
+    elif mark in B:
+        return 'B'
+    elif mark in C:
+        return 'C'
+    elif mark in D:
+        return 'D'
+    else:
+        return 'F'
--- test_letter.py
+++ test_letter.py
@@ -5,7 +5,26 @@ import letter
 class TestMark(unittest.TestCase):
 
     def test_mark_A(self):
-        result = letter.letter_grade(80)
-        self.assertEqual(result,"A")
-        
+       for i in range(80,101):
+          self.assertEqual(letter.letter_grade(i),"A")
 
+    def test_mark_B(self):
+       for i in range(70,80):
+          self.assertEqual(letter.letter_grade(i),"B")
+
+    def test_mark_C(self):
+       for i in range(60,70):
+          self.assertEqual(letter.letter_grade(i),"C")
+
+    def test_mark_D(self):
+       for i in range(50,60):
+          self.assertEqual(letter.letter_grade(i),"D")
+
+    def test_mark_F(self):
+       for i in range(0,50):
+          self.assertEqual(letter.letter_grade(i),"F")
+
+
+
+if __name__== '__main__':
+    unittest
